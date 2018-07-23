3ce6ae97dd4424a5b3894175143fb2af304abb0b
TrevorSeitz <trevinla@gmail.com>
2018-07-23 10:02:58 -0400

    added routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  resources :dogs, :users, :welcome, :sessions, :join_walks, :walks
+
+  resources :users do
+    resources :dogs, :walks
+  end
+
+  resources :dogs do
+    resources :walks
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  get '/auth/github/callback' => 'sessions#create'
+  get '/signout' => 'sessions#destroy', :as => :signout
 end
