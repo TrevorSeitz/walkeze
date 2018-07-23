52372d1de8f24225f503fe1deb5784634dd587e9
TrevorSeitz <trevinla@gmail.com>
2018-07-23 09:44:10 -0400

    users controller added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    # @user = current_user
+
+    @user = User.find(session[:user_id])
+    @dogs = Dog.where(user_id: @user.id)
+  end
+
+end
